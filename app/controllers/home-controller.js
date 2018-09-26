d8c1ae8c947f4e8c95cdda52fd82873ca1c82d9b
manfredmarcano <manfredalberto@gmail.com>
2018-09-26 05:29:08 -0300

    Prueba realizada

--- app/controllers/home-controller.js
+++ app/controllers/home-controller.js
@@ -1,3 +1,39 @@
 app.controller("homeController", [function(){
+    var vm = this;
+
+    vm.isCollapsed = true;
+
+    vm.box = {
+        title: 'Título Caja',
+        list: {
+            title: 'Lorem Ipsum',
+            items: [
+                'Dolor sit Amet, consectetur adipiscing elit',
+                'Dolor sit Amet, consectetur adipiscing elit',
+                'Dolor sit Amet, consectetur adipiscing elit',
+                'Dolor sit Amet, consectetur adipiscing elit',
+            ]
+        },
+        pricesList: [
+            {
+                description: 'Dolor sit Amet, consectetur adipiscing elit. Duis non tincidunt nulla. Morbi at sapien viverra, porttitor eros a',
+                price: '2000000000000000000'
+            },
+            {
+                description: 'Dolor sit Amet, consectetur adipiscing elit.',
+                price: '2000000000000000000'
+            }
+        ]
+    };
+
+    vm.dateFrom = null;
+    vm.dateTo = null;
+    vm.datepickerOptions = {
+        minDate: new Date('2010-05-01'),
+        initDate: new Date()
+    };
+
+    vm.totalItems = 64;
+    vm.currentPage = 4;
 
 }]);
